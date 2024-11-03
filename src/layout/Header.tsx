0a3cd79f10a90c6d5cac7f0f2507acdd0ac3a74e
Jefferson Miranda <jefferson472@hotmail.com>
2024-11-03 12:21:37 -0300

    define cor de fundo e adiciona links

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,14 +3,15 @@ export default function Header() {
     <header className="
       fixed top-0 w-[calc(100%-(var(--padding-default)*2))]
       max-w-[calc(var(--max-width-default)-(var(--padding-default)*2))]
+      bg-primary
     ">
       <div className="flex items-center text-xl h-[var(--header-height)]">
         <div className="flex flex-grow justify-start"><a href="/">LOGO</a></div>
         <nav>
           <ul className="flex justify-end gap-8">
-            <li>Início</li>
+            <li><a href="#presentation-section">Início</a></li>
             <li>Sobre</li>
-            <li>Projetos</li>
+            <li><a href="#projects-section">Projetos</a></li>
             <li>Fale Comigo</li>
           </ul>
         </nav>
