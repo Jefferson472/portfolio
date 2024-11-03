9e1471a2972ef8511f340510fda50c862b18390b
Jefferson Miranda <jefferson472@hotmail.com>
2024-11-03 12:21:23 -0300

    ajusta espaçamento da seção projetos

--- src/layout/Sections/Projects.tsx
+++ src/layout/Sections/Projects.tsx
@@ -1,21 +1,25 @@
 import Card from "../../components/Card";
+import Spacer from "../../components/Spacer";
 
 export default function Projects() {
   return (
-    <section id="projects-section"
-      className="
-        flex justify-center
-        h-[calc(100vh-var(--header-height))] w-full
-      "
-    >
-      <div>
-        <h1>Meus Projetos</h1>
-        <div className="grid grid-cols-3 gap-4">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <Card key={index} />
-          ))}
+    <>
+      <Spacer height={80}></Spacer>
+      <section id="projects-section"
+        className="
+          flex justify-center
+          h-[calc(100vh-)] w-full
+        "
+      >
+        <div>
+          <h1>Meus Projetos</h1>
+          <div className="grid grid-cols-3 gap-4 mt-8">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <Card key={index} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
