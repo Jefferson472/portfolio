9ee6d4372b11f291cf58adbe8e5cd884c6151986
Jefferson Miranda <jefferson472@hotmail.com>
2024-11-03 08:06:42 -0300

    corrige o tamanho e a altura do header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <header className="
-      fixed top-0 w-full 
+      fixed top-0 w-[calc(100%-(var(--padding-default)*2))]
       max-w-[calc(var(--max-width-default)-(var(--padding-default)*2))]
     ">
-      <div className="flex items-center h-20 text-xl">
+      <div className="flex items-center text-xl h-[var(--header-height)]">
         <div className="flex flex-grow justify-start"><a href="/">LOGO</a></div>
         <nav>
           <ul className="flex justify-end gap-8">
