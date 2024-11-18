75b92c3d739c8bd3aa7ecace0f6059abe6e353b5
Jefferson Miranda <jefferson472@hotmail.com>
2024-11-18 09:08:02 -0300

    transforma o formato da imagem em círculo

--- src/layout/Sections/Presentation.tsx
+++ src/layout/Sections/Presentation.tsx
@@ -11,8 +11,8 @@ export default function Presentation() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis orci a porta eleifend. Vivamus ultrices convallis justo, at dictum erat consectetur quis. Nullam nec aliquet lacus. Nullam ultricies nulla dolor, eget blandit lorem scelerisque eu. In hac habitasse platea dictumst. Nullam accumsan arcu vel auctor dapibus. Nulla convallis risus a lacus pellentesque viverra. In nec nunc sed velit placerat mollis.</p>
       </div>
 
-      <div className="flex items-center w-[30%]">
-        <img src="src\assets\img\jefferson-profile.png" width="1000" />
+      <div className="flex items-center w-[25%]">
+        <img className="rounded-full shadow-lg" src="src\assets\img\jefferson-profile.png" />
       </div>
     </section>
   )
