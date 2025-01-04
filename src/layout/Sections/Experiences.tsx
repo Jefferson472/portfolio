ba7edd37f0639ceab0add53464300039efd7afd3
Jefferson Miranda <jefferson472@hotmail.com>
2025-01-04 11:12:53 -0300

    altera experiências para ler as informações do data experiences

--- src/layout/Sections/Experiences.tsx
+++ src/layout/Sections/Experiences.tsx
@@ -1,6 +1,13 @@
 import Spacer from "../../components/Spacer";
+import experiences from '../../data/experiences.json';
 
 export default function Projects() {
+  const dateConvert = (date: string) => {
+    const months = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+    const [year, month] = date.split("-");
+    return `${months[parseInt(month) - 1]} de ${year}`;
+  };
+
   return (
     <>
       <Spacer height={80}></Spacer>
@@ -8,15 +15,15 @@ export default function Projects() {
         className="min-h-[calc(100vh-(var(--padding-default)))] w-full"
       >
         <Spacer height={80}></Spacer>
-        <div className="flex flex-col items-center justify-between min-h-[calc(100vh-var(--header-height))] pb-4">
-          <h2 className="text-4xl font-semibold mb-4 text-center">Experiência</h2>
+        <div className="flex flex-col items-center justify-around min-h-[calc(100vh-var(--header-height))] pb-4">
+          <h2 className="text-4xl font-semibold mb-4 text-center">Experiências</h2>
           <div className="flex flex-col gap-6 mt-8 max-w-[672px] mx-auto">
-            {Array.from({ length: 3 }).map((_, index) => (
+            {experiences.reverse().map(xp => (
               <div className="flex gap-3">
                 <div className="hidden sm:flex flex-col items-center">
                   <div className="flex gap-3 items-center min-h-[28px]">
                     <span className="inline-block bg-gray-500 text-white text-xs font-semibold px-2.5 py-0.5 rounded relative">
-                      2024
+                      {xp.start.split("-")[0]}
                       <span className="absolute top-1/2 right-0 transform translate-x-full -translate-y-1/2 border-t-4 border-b-4 border-l-4 border-transparent border-l-gray-500"></span>
                     </span>
                   </div>
@@ -30,11 +37,12 @@ export default function Projects() {
                 </div>
 
                 <div className="flex flex-col justify-start items-start ml-5 text-justify">
-                  <h2 className="">Nome do Cargo</h2>
-                  <p className="">Nome da Empresa<span className="sm:hidden text-secondary"> | 2024</span></p>
-                  <p className="text-small">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at blandit lacus. Donec ac aliquet velit, sed mattis turpis. Aliquam at elementum metus. In volutpat luctus felis, vitae pharetra purus ullamcorper egestas. In volutpat blandit ante vel malesuada. Etiam auctor, libero ornare ultricies ultricies, turpis odio auctor nunc, sed consequat felis enim eget erat. Etiam viverra eros tristique risus placerat pulvinar. Fusce vulputate auctor risus, vitae tristique dui. Fusce diam metus, viverra ut sem non, tincidunt auctor turpis. Donec vel massa at mi interdum mattis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean eleifend felis non tellus imperdiet placerat. Pellentesque commodo orci at velit molestie, a dignissim elit dignissim.
+                  <h2 className="">{xp.name}</h2>
+                  <p className="">
+                    {xp.company}
+                    <span className="text-secondary"> | {dateConvert(xp.start)} - {xp.end ? dateConvert(xp.end) : "o momento"}</span>
                   </p>
+                  <p className="text-small">{xp.description}</p>
                 </div>
               </div>
             ))}
